0d7f38ede04adc4f6e8e2ffc6ac1849c737917bb
vysarge <vysarge@gmail.com>
2017-04-25 20:30:41 -0400

    training and testing infrastructure

--- exposure_cnn/process_image.py
+++ exposure_cnn/process_image.py
@@ -80,70 +80,102 @@ def fileList(filepath):
         filelist.extend(fullfilenames)
     return filelist
 
-with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-    currentDirectory = 'S:\\Dropbox (Personal)\\Assignments\\6.344\\project\\6344-project\\exposure_cnn\\'
-    trainshape = (32, 32, 1, 3)
-    x = tf.placeholder('float32', shape=trainshape)
-    l = tf.placeholder('float32', shape=trainshape)
-    y = buildModel(x)
-    loss = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(l, y))))
-    tf.summary.scalar('loss', loss)
-    optimizer = tf.train.GradientDescentOptimizer(0.00000005)
-    train_op = optimizer.minimize(loss)
-    
-    input_filepath = 'phos\\0\\'
-    label_filepath = 'phos\\plus_2\\'
-    inputs = fileList(input_filepath)
-    #labels = fileList(label_filepath)
-    #inputs = os.listdir(input_filepath)
-    #labels = os.listdir(label_filepath)
+def runTrain(loadModel, loadName='', saveName=''):
+    with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
+        currentDirectory = 'S:\\6344-project\\exposure_cnn\\'
+        trainshape = (32, 32, 1, 3)
+        norm = 32*32*1*3
+        x = tf.placeholder('float32', shape=trainshape)
+        l = tf.placeholder('float32', shape=trainshape)
+        y = buildModel(x)
+        loss = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(l, y)))/norm) # RMSE
+        tf.summary.scalar('loss', loss)
 
-    #with tf.Graph().as_default():
-    init_op = tf.global_variables_initializer()
-    saver = tf.train.Saver()
-    sess.run(init_op)
-    
-    feed_dict = {}
-    step = 0
-    print('Starting')
-    for input_file in inputs:
-        fileparts = input_file.split('\\')
-        scene = fileparts[-2]
-        file = fileparts[-1] # the last element of this split should be the file name
-        label_file = label_filepath + scene + '\\' + file
-        if (os.path.exists(label_file)):
-            im_in = Image.open(input_file)
-            h, w = im_in.size
-            im_in = np.asarray(im_in).astype('float32')
-            im_in = im_in.reshape([h, w, 1, 3]);
-            #im_inputs = setFromImage(im_in)
+        learning_rate_init = 0.000001
+        decay_steps = 10000
+        decay_rate = 0.9
+        global_step = tf.Variable(0, trainable=False)
+        learning_rate = learning_rate_init#tf.train.exponential_decay(learning_rate_init, global_step, decay_steps, decay_rate)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+        train_op = optimizer.minimize(loss)
+        
+        input_filepath = 'phos\\0\\'
+        label_filepath = 'phos\\plus_2\\'
+        inputs = fileList(input_filepath)
+        #labels = fileList(label_filepath)
+        #inputs = os.listdir(input_filepath)
+        #labels = os.listdir(label_filepath)
 
-            im_la = Image.open(label_file)
-            h, w = im_la.size
-            im_la = np.asarray(im_la).astype('float32')
-            im_la = im_la.reshape([h, w, 1, 3]);
-            #im_labels = setFromImage(im_la)
-            _, loss_value = sess.run([train_op, loss], feed_dict={x:im_in, l:im_la})
-            if (step % 100 == 0):
-                print("Step: {}, Loss: {}".format(step, loss_value))
-            step = step + 1
+        #with tf.Graph().as_default():
+        init_op = tf.global_variables_initializer()
+        saver = tf.train.Saver()
+
+        if loadModel:
+            saver.restore(sess, loadName)
+            print('Load model from ' + loadName)
         else:
-            print('From {}: {} does not exist'.format(input_file, label_file))
+            sess.run(init_op)
+            print('Initializing fresh variables')
 
-    saver.save(sess, currentDirectory + 'model.ckpt') # save model checkpoint
+        step = 0
+        feed_dict = {}
+        for input_file in inputs:
+            fileparts = input_file.split('\\')
+            scene = fileparts[-2]
+            file = fileparts[-1] # the last element of this split should be the file name
+            label_file = label_filepath + scene + '\\' + file
+            if (os.path.exists(label_file)):
+                im_in = Image.open(input_file)
+                h, w = im_in.size
+                im_in = np.asarray(im_in).astype('float32')
+                im_in = im_in.reshape([h, w, 1, 3]);
+                #im_inputs = setFromImage(im_in)
+
+                im_la = Image.open(label_file)
+                h, w = im_la.size
+                im_la = np.asarray(im_la).astype('float32')
+                im_la = im_la.reshape([h, w, 1, 3]);
+                #im_labels = setFromImage(im_la)
+                _, loss_value = sess.run([train_op, loss], feed_dict={x:im_in, l:im_la})
+                #print(global_step.eval(session=sess))
+                #step = global_step.eval(session=sess)
+                if (step % 100 == 0):
+                    print("Step: {}, Loss: {}, Rate: {}".format(step, loss_value, learning_rate))
+                #global_step = global_step + 1
+                step = step + 1
+            else:
+                print('From {}: {} does not exist'.format(input_file, label_file))
+
+        saver.save(sess, currentDirectory + saveName) # save model checkpoint
     #im = Image.open('input.bmp')
     #h, w = im.size
     #im_te = np.asarray(im).astype('float32')
     #im_in =  im_te.reshape([h, w, 1, 3]);
     
 
+def processImage(modelName, imageName):
+    im = Image.open(imageName)
+    print(im.size)
+    h, w = im.size
+    im_te = np.asarray(im).astype('float32')
+    im_in =  im_te.reshape([h, w, 1, 3]);
     
+    x = tf.placeholder('float32', np.shape(im_in))
+    print(np.shape(im_in))
+    print(x)
+    y = buildModel(x)
 
     
-    
-    
 
-    
+    with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
+        saver = tf.train.Saver()
+        saver.restore(sess, modelName)
+        print('Load model from ' + modelName)
+        
+        im_out = y.eval(session=sess,feed_dict={x:im_in})
+        im_out = im_out.reshape([w, h, 3]).clip(0, 255).astype('uint8')
+    im_o = Image.fromarray(im_out)
+    im_o.save('output.bmp')
     #im_out = y.eval(session=sess,feed_dict={x:im_in})
     #im_out = im_out.reshape([h, w, 3]).astype('uint8')
     #print(np.shape(im_out))
@@ -151,3 +183,12 @@ with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
 #im_o = Image.fromarray(im_out)
 
 #im_o.save('output.bmp')
+modelName = 'model\\model.ckpt'
+#runTrain(False, saveName=modelName)
+#runTrain(True, loadName=modelName, saveName=modelName)
+
+scene = 15
+exposure = 0
+phos_path = 'C:\\Users\\vysarge\\Documents\\hdr_dataset\\Phos2_3MP\\Phos2_scene{}\\'.format(scene)
+test_name = 'Phos2_uni_sc{}_{}.png'.format(scene, exposure)
+processImage(modelName, phos_path + test_name)
