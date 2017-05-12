9f23b16aafb4af7f50fa01c6cab15f98b1b32732
vysarge <vysarge@gmail.com>
2017-05-12 19:37:20 -0400

    Added more metrics

--- matlab/getMetrics.m
+++ matlab/getMetrics.m
@@ -52,9 +52,21 @@ plus_1x1_we = well_exposedness(mat2gray(plus_1x1_img));
 plus_1x1_we_str = ['(' num2str(mean(plus_1x1_we(:))) ', ' num2str(var(plus_1x1_we(:))) ')'];
 plus_3x3_we = well_exposedness(mat2gray(plus_3x3_img));
 plus_3x3_we_str = ['(' num2str(mean(plus_3x3_we(:))) ', ' num2str(var(plus_3x3_we(:))) ')'];
+
+fused_1x1_we = well_exposedness(mat2gray(fused_1x1_img));
+fused_1x1_we_str = ['(' num2str(mean(fused_1x1_we(:))) ', ' num2str(var(fused_1x1_we(:))) ')'];
+fused_3x3_we = well_exposedness(mat2gray(fused_3x3_img));
+fused_3x3_we_str = ['(' num2str(mean(fused_3x3_we(:))) ', ' num2str(var(fused_3x3_we(:))) ')'];
+mkhdr_1x1_we = well_exposedness(mat2gray(mkhdr_1x1_img));
+mkhdr_1x1_we_str = ['(' num2str(mean(mkhdr_1x1_we(:))) ', ' num2str(var(mkhdr_1x1_we(:))) ')'];
+mkhdr_3x3_we = well_exposedness(mat2gray(mkhdr_3x3_img));
+mkhdr_3x3_we_str = ['(' num2str(mean(mkhdr_3x3_we(:))) ', ' num2str(var(mkhdr_3x3_we(:))) ')'];
+
 disp('CNN-estimated well-exposedness (mean, variance)');
 disp(['Estimated under-exposed: 1x1 = ' minus_1x1_we_str '; 3x3 = ' minus_3x3_we_str]);
 disp(['Estimated over-exposed: 1x1 = ' plus_1x1_we_str '; 3x3 = ' plus_3x3_we_str]);
+disp(['Exposure fusion outputs: 1x1 = ' fused_1x1_we_str '; 3x3 = ' fused_3x3_we_str]);
+disp(['makeHDR function outputs: 1x1 = ' mkhdr_1x1_we_str '; 3x3 = ' mkhdr_3x3_we_str]);
 
 
 %% SSIM
@@ -83,10 +95,10 @@ fused_res = hdrvdp(mat2gray(fused_img), mat2gray(ref_hdr), 'sRGB-display', 30);
 mkhdr_res = hdrvdp(mat2gray(mkhdr_img), mat2gray(ref_hdr), 'sRGB-display', 30);
 
 % CNN (exposure) output images
-fused_1x1_res = hdrvdp(mat2gray(input_img), mat2gray(fused_1x1_img), 'sRGB-display', 30);
-fused_3x3_res = hdrvdp(mat2gray(input_img), mat2gray(fused_3x3_img), 'sRGB-display', 30);
-mkhdr_1x1_res = hdrvdp(mat2gray(input_img), mat2gray(mkhdr_1x1_img), 'sRGB-display', 30);
-mkhdr_3x3_res = hdrvdp(mat2gray(input_img), mat2gray(mkhdr_3x3_img), 'sRGB-display', 30);
+fused_1x1_res = hdrvdp(mat2gray(fused_1x1_img), mat2gray(ref_hdr), 'sRGB-display', 30);
+fused_3x3_res = hdrvdp(mat2gray(fused_3x3_img), mat2gray(ref_hdr), 'sRGB-display', 30);
+mkhdr_1x1_res = hdrvdp(mat2gray(mkhdr_1x1_img), mat2gray(ref_hdr), 'sRGB-display', 30);
+mkhdr_3x3_res = hdrvdp(mat2gray(mkhdr_3x3_img), mat2gray(ref_hdr), 'sRGB-display', 30);
 
 imwrite(fused_1x1_res.P_map, fullfile(exp_outputs_dir, 'fused_1x1_pmap.png'));
 imwrite(fused_3x3_res.P_map, fullfile(exp_outputs_dir, 'fused_3x3_pmap.png'));
